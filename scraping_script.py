9cbc4041375bb164d887e3e8e9a271c900bed365
Victor Gaspar <victorgaspar1@hotmail.com>
2023-06-07 10:02:35 -0300

    teste 9

--- scraping_script.py
+++ scraping_script.py
@@ -8,9 +8,22 @@ from bs4 import BeautifulSoup
 from google.cloud import storage
 import re
 import mysql.connector as bd
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
 
 cnx = bd.connect(user='root', password = 'root', host = '104.154.155.31', database = 'Times')
 conn = cnx.cursor()
+
+credentials = GoogleCredentials.get_application_default()
+
+service = discovery.build('sqladmin', 'v1beta4', credentials=credentials)
+
+# Project ID of the project that contains the instance.
+project = 'DataOps'  # TODO: Update placeholder value.
+
+# Database instance ID. This does not include the project ID.
+instance = 'root'  # TODO: Update placeholder value.
+
 
 def scrape_this(uri="/pages/forms/"):
   page = requests.get("https://scrapethissite.com" + uri)
@@ -48,5 +61,11 @@ hockey_team_df = pd.concat(temp_dfs, axis=0).reset_index()
 hockey_team_df.sort_values(["year", "name"], inplace=True)
 
 for time in hockey_team_df.values:
-  query = f"insert into times values({time[0]},{time[1]},{time[3]},{time[4]})"
-  conn.execute(query)  
+  database_body = {
+    'id':time[0],
+    'nome':time[1],
+    'vitoria':time[3],
+    'derrota':time[4]
+}
+  request = service.databases().insert(project=project, instance=instance, body=database_body)
+  response = request.execute()
